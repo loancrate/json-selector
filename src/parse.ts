9781ba2515515931484fcf7b6940a5d7a0c1e833
Trevor Robinson <trevor@loancrate.com>
2026-02-13 04:35:18 -0600

    LC-16557: Parser support for JMESPath functions, multi-select, and projections (#18)
    Extend the parser to handle new JMESPath syntax:
    - Function calls: name(args) with dot syntax foo.func(args)
    - Multi-select lists: [expr1, expr2, ...] and dot syntax foo.[...]
    - Multi-select hashes: {key: expr, ...} and dot syntax foo.{...}
    - Object projections: .* with projection continuation
    - Expression references: &expr for higher-order functions
    - Keyword literals: true, false, null without backtick syntax

--- src/parse.ts
+++ src/parse.ts
@@ -1,6 +1,7 @@
 import { JsonSelector } from "./ast";
 import { Parser } from "./parser";
 
+/** Parses a selector expression string into an AST that can be evaluated, formatted, or compiled into an accessor. */
 export function parseJsonSelector(selectorExpression: string): JsonSelector {
   const parser = new Parser(selectorExpression);
   return parser.parse();
